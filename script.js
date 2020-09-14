d62f6132060f924a794ad3dfe39c8aa988951fbc
alligatormonday <joey@joeyjepson.com>
2020-09-14 16:48:01 -0700

    Started to work my JavaScript. Added my APIKey and ajax call.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
     console.log("document loaded");
 
+    // let APIKey = "ce818fa75a8998632b2becab04bcb211";
+
+    let queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=ce818fa75a8998632b2becab04bcb211";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function (response) {
+
+        // Log the queryURL
+        console.log(queryURL);
+
+        // Log the resulting object
+        console.log(response);
+
+        
+    });
 
 
 
