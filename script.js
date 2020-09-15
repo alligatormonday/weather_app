2d3e8236aefea5e07860557ab31347d372fa0918
alligatormonday <joey@joeyjepson.com>
2020-09-14 19:11:17 -0700

    Made ajax call functional and responsive to queryURL and APIKey for 'Tempe, AZ'. Displayed City Name, Wind Speed, Humidity, and Temp on page using jQuery.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
     console.log("document loaded");
 
-    // let APIKey = "ce818fa75a8998632b2becab04bcb211";
 
-    let queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=ce818fa75a8998632b2becab04bcb211";
+let APIKey = "ce818fa75a8998632b2becab04bcb211";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
+// Here we are building the URL we need to query the database
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=Tempe,Arizona&appid=" + APIKey;
+
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
     .then(function (response) {
 
         // Log the queryURL
@@ -17,43 +21,41 @@ $(document).ready(function () {
         // Log the resulting object
         console.log(response);
 
-        
+        $("#city").html("<h1>" + response.name + "</h1>");
+        $("#wind").text("Wind Speed: " + response.wind.speed);
+        $("#humidity").text("Humidity: " + response.main.humidity);
+        // $("#uv"),text("UV Index " + response.)
+
+        // Convert the temp to fahrenheit
+        let temp = (response.main.temp - 273.15) * 1.80 + 32;
+
+        // add temp content to html
+        // $("#temp").text("Temperature (K) " + response.main.temp);
+        $("#temp").text("Temperature (F) " + temp.toFixed(2));
+
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + temp);
+
+
     });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
+
