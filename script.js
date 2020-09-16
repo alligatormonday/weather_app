b6417c626242a728e88afbbe40cbdb99e3a8210f
alligatormonday <joey@joeyjepson.com>
2020-09-15 22:30:32 -0700

    Added 'MPH' wind speed response  and '℉' to temperature response.

--- script.js
+++ script.js
@@ -1,84 +1,83 @@
 $(document).ready(function () {
-    console.log("document loaded");
+  console.log("document loaded");
 
+  let APIKey = "ce818fa75a8998632b2becab04bcb211";
 
-let APIKey = "ce818fa75a8998632b2becab04bcb211";
+  let location = "Tempe";
 
-let location = "Tempe";
+  // Here we are building the URL we need to query the database
+  let queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location +
+    "&appid=" +
+    APIKey;
 
-// Here we are building the URL we need to query the database
-let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + APIKey;
-
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
     url: queryURL,
-    method: "GET"
-})
-    .then(function (response) {
+    method: "GET",
+  }).then(function (response) {
+    // Log the queryURL
+    console.log(queryURL);
 
-        // Log the queryURL
-        console.log(queryURL);
+    // Log the resulting object
+    console.log(response);
 
-        // Log the resulting object
-        console.log(response);
+    $("#city").html("<h1>" + response.name + "</h1>");
+    $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
+    $("#humidity").text("Humidity: " + response.main.humidity + "%");
 
-        $("#city").html("<h1>" + response.name + "</h1>");
-        $("#wind").text("Wind Speed: " + response.wind.speed);
-        $("#humidity").text("Humidity: " + response.main.humidity);
-        
+    // Convert the temp to fahrenheit
+    let temp = (response.main.temp - 273.15) * 1.8 + 32;
 
-        // Convert the temp to fahrenheit
-        let temp = (response.main.temp - 273.15) * 1.80 + 32;
+    // add temp content to html
+    // $("#temp").text("Temperature (K) " + response.main.temp);
+    $("#temp").text("Temperature: " + temp.toFixed(2) + " ℉");
 
-        // add temp content to html
-        // $("#temp").text("Temperature (K) " + response.main.temp);
-        $("#temp").text("Temperature (F) " + temp.toFixed(2));
+    // Log the data in the console as well
+    console.log("Wind Speed: " + response.wind.speed);
+    console.log("Humidity: " + response.main.humidity);
+    console.log("Temperature: " + temp);
 
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + temp);
+    let uvQuery =
+      "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+      APIKey +
+      "&lat=" +
+      response.coord.lat +
+      "&lon=" +
+      response.coord.lon;
+    console.log(uvQuery);
 
-        let uvQuery = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon;
-        console.log(uvQuery)
-        
-        $.ajax({
-            url: uvQuery,
-            method: "GET"
-        })
-        .then(function(res){
-            console.log(res)
-            $("#uv").text("UV Index: " + res.value);
-            console.log("UV")
+    $.ajax({
+      url: uvQuery,
+      method: "GET",
+    }).then(function (res) {
+      console.log(res);
+      $("#uv").text("UV Index: " + res.value);
+      console.log("UV");
 
-        let fiveDayQuery = "http://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=" + APIKey;
-        console.log(fiveDayQuery)
+      let fiveDayQuery =
+        "http://api.openweathermap.org/data/2.5/forecast?q=" +
+        location +
+        "&appid=" +
+        APIKey;
+      console.log(fiveDayQuery);
 
-        $.ajax({
-            url: fiveDayQuery,
-            method: "GET"
-        })
-        .then(function(forecast){
-            console.log(forecast)
+      $.ajax({
+        url: fiveDayQuery,
+        method: "GET",
+      }).then(function (forecast) {
+        console.log(forecast);
 
-            let list = forecast.list;
-        
-            for (let i = 7; i < list.length; i+8){
-                // append forecast box
+        let list = forecast.list;
 
-            }
-        })
-        })
-
-        
-       
-
-
+        for (let i = 7; i < list.length; i + 8) {
+          // append forecast box
+        }
+      });
     });
-
-
-
-})
+  });
+});
 
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
@@ -93,4 +92,3 @@ $.ajax({
 // THEN I am again presented with current and future conditions for that city
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
-
